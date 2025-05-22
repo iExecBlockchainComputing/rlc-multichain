6ec3c063bbd142e5d4dd465d29a00b1501dd05c9
Zied <26070035+zguesmi@users.noreply.github.com>
2025-05-22 19:12:29 +0200

    Format

--- test/utils/RLCAdapterTestSetup.sol
+++ test/utils/RLCAdapterTestSetup.sol
@@ -5,7 +5,6 @@ import {Test, console} from "forge-std/Test.sol";
 import {Deploy as RLCAdapterDeploy} from "../../script/RLCAdapter.s.sol";
 
 contract RLCAdapterTestSetup is Test {
-
     function _forkSepoliaAndDeploy() internal returns (address) {
         vm.createSelectFork(vm.envString("SEPOLIA_RPC_URL"));
         return new RLCAdapterDeploy().run();
