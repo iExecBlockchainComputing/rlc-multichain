4a32a1d7135ac7504934259898833f6f27af8c22
Zied Guesmi <26070035+zguesmi@users.noreply.github.com>
2025-05-23 16:47:09 +0200

    feat: Init unit tests (#7)

---
+++ test/utils/RLCOFTTestSetup.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Deploy as RLCOFTDeploy} from "../../script/RLCOFT.s.sol";
+
+contract RLCOFTTestSetup is Test {
+    function _forkArbitrumTestnetAndDeploy() internal returns (address) {
+        vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC_URL"));
+        return new RLCOFTDeploy().run();
+    }
+}
