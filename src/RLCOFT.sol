47d40ab4af89e842a98eab8110b7d734f7b39f35
Robin Le Caignec <72495599+Le-Caignec@users.noreply.github.com>
2025-05-28 17:34:24 +0200

    Update src/RLCOFT.sol
    Co-authored-by: Zied Guesmi <26070035+zguesmi@users.noreply.github.com>

--- src/RLCOFT.sol
+++ src/RLCOFT.sol
@@ -79,7 +79,7 @@ contract RLCOFT is OFTUpgradeable, UUPSUpgradeable, AccessControlDefaultAdminRul
 
     /**
      * @dev See {ERC20-_update}.
-     * The following functions are overrides required by Solidity.
+     * Override this functions to prevent its execution when the contract is paused.
      */
     function _update(address from, address to, uint256 value) internal virtual override whenNotPaused {
         super._update(from, to, value);
