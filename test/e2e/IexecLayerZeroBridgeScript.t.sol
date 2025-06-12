a02b4c34605615bb59a030a4f593fde751f25764
Le-Caignec <robinlecaignec@yahoo.fr>
2025-06-12 11:08:04 +0200

    fix: correct variable name for RLCMock instance in IexecLayerZeroBridgeScriptTest

--- test/e2e/IexecLayerZeroBridgeScript.t.sol
+++ test/e2e/IexecLayerZeroBridgeScript.t.sol
@@ -7,9 +7,7 @@ import {IexecLayerZeroBridge} from "../../src/IexecLayerZeroBridge.sol";
 //TODO: To remove when RLC_ERC20 contract will be developed
 import {RLCMock} from "../units/mocks/RLCMock.sol";
 
-contract IexecLayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroBridgeScriptTest is
-    Test
-{
+contract IexecLayerZeroBridgeScriptTest is Test {
     // Unique instance of the deployment script
     address owner = makeAddr("OWNER_ADDRESS");
     address pauser = makeAddr("PAUSER_ADDRESS");
@@ -22,7 +20,7 @@ contract IexecLayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroB
         vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC_URL")); // use public node
         deployer = new IexecLayerZeroBridgeDeploy();
         //TODO: To remove when RLC_ERC20 contract will be developed
-        rlcChainX = address(new RLCMock("TokenChainX", "RLC"));
+        rlcAddress = address(new RLCMock("TokenChainX", "RLC"));
         vm.setEnv("CREATE_X_FACTORY", "0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed");
     }
 
@@ -30,10 +28,10 @@ contract IexecLayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroB
     function testFork_CheckDeployment() public {
         bytes32 salt = keccak256("IexecLayerZeroBridge_SALT");
         IexecLayerZeroBridge iexecLayerZeroBridge =
-            IexecLayerZeroBridge(deployer.deploy(rlcChainX, LAYERZERO_ENDPOINT, owner, pauser, salt));
+            IexecLayerZeroBridge(deployer.deploy(rlcAddress, LAYERZERO_ENDPOINT, owner, pauser, salt));
 
         assertEq(iexecLayerZeroBridge.owner(), owner);
-        assertEq(iexecLayerZeroBridge.token(), address(rlcChainX));
+        assertEq(iexecLayerZeroBridge.token(), address(rlcAddress));
         // TODO check all roles.
         // TODO check that the contract is not paused by default.
         // TODO check that the contract has been initialized and cannot be re-initialized.
@@ -44,19 +42,19 @@ contract IexecLayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroBridgeayerZeroB
     function testForkFuzz_DifferentSaltsProduceDifferentAddresses(bytes32 salt1, bytes32 salt2) public {
         vm.assume(salt1 != salt2); // ensure they are different
 
-        address addr1 = deployer.deploy(rlcChainX, LAYERZERO_ENDPOINT, owner, pauser, salt1);
-        address addr2 = deployer.deploy(rlcChainX, LAYERZERO_ENDPOINT, owner, pauser, salt2);
+        address addr1 = deployer.deploy(rlcAddress, LAYERZERO_ENDPOINT, owner, pauser, salt1);
+        address addr2 = deployer.deploy(rlcAddress, LAYERZERO_ENDPOINT, owner, pauser, salt2);
 
         assertTrue(addr1 != addr2, "Fuzz test failed: different salts produced same address");
     }
 
     function testForkFuzz_RevertIfSecondDeploymentWithSameSalt(bytes32 salt) public {
         // First deployment
-        address addr = deployer.deploy(rlcChainX, LAYERZERO_ENDPOINT, owner, pauser, salt);
+        address addr = deployer.deploy(rlcAddress, LAYERZERO_ENDPOINT, owner, pauser, salt);
         assertTrue(addr != address(0), "First deployment should succeed");
 
         // Attempt redeployment with the same salt
-        try deployer.deploy(rlcChainX, LAYERZERO_ENDPOINT, owner, pauser, salt) returns (address) {
+        try deployer.deploy(rlcAddress, LAYERZERO_ENDPOINT, owner, pauser, salt) returns (address) {
             revert("Expected revert on redeployment with same salt but no revert occurred");
         } catch {
             // Expected: revert due to CREATE2 address collision
