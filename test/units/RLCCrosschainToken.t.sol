b6c9016ee4e9a26e556a2cdee1d612d685b4b050
Zied <26070035+zguesmi@users.noreply.github.com>
2025-06-13 21:35:12 +0200

    Add unit tests

---
+++ test/units/RLCCrosschainToken.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {Test, console} from "forge-std/Test.sol";
+import {CreateX} from "@createx/contracts/CreateX.sol";
+import {Deploy as RLCCrosschainTokenDeployScript} from "../../script/RLCCrosschainToken.s.sol";
+import {RLCCrosschainToken} from "../../src/token/RLCCrosschainToken.sol";
+
+contract RLCCrosschainTokenTest is Test {
+
+    error InvalidInitialization();
+    error AccessControlUnauthorizedAccount(address account, bytes32 role);
+
+    address private owner = makeAddr("owner");
+    address private upgrader = makeAddr("upgrader");
+
+    RLCCrosschainToken private crossChainToken;
+
+    function setUp() public {
+        crossChainToken = RLCCrosschainToken(
+            new RLCCrosschainTokenDeployScript().deploy(
+                "RLC Token",
+                "RLC",
+                owner,
+                upgrader,
+                address(new CreateX()),
+                keccak256("salt")
+            )
+        );
+    }
+
+    function test_RevertWhenInitializedMoreThanOnce() public {
+        vm.expectRevert(InvalidInitialization.selector);
+        crossChainToken.initialize("Foo", "BAR", owner, upgrader);
+    }
+
+    function test_RevertWhenUnauthorizedUpgrader() public {
+        address unauthorizedUpgrader = makeAddr("unauthorized");
+        vm.expectRevert(
+            abi.encodeWithSignature(
+                "AccessControlUnauthorizedAccount(address,bytes32)",
+                unauthorizedUpgrader,
+                crossChainToken.UPGRADER_ROLE()
+            )
+        );
+        vm.prank(unauthorizedUpgrader);
+        crossChainToken.upgradeToAndCall(makeAddr("newImpl"), "");
+    }
+}
