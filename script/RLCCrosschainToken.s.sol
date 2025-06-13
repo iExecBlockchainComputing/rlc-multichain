57155b0b6cf49ab394d31b188fcb2948e5bc03d8
Zied <26070035+zguesmi@users.noreply.github.com>
2025-06-13 21:54:12 +0200

    Clean

--- script/RLCCrosschainToken.s.sol
+++ script/RLCCrosschainToken.s.sol
@@ -8,25 +8,45 @@ import {Script} from "forge-std/Script.sol";
 import {RLCCrosschainToken} from "../src/token/RLCCrosschainToken.sol";
 import {UUPSProxyDeployer} from "./lib/UUPSProxyDeployer.sol";
 
+/**
+ * Deployment script for the RLCCrosschainToken contract.
+ * It reads configuration from a JSON file and deploys the contract using CreateX.
+ */
 contract Deploy is Script {
     using stdJson for string;
 
+    /**
+     * Reads configuration from a JSON file and deploys RLCCrosschainToken contract.
+     *
+     * @return address of the deployed RLCCrosschainToken proxy contract.
+     */
     function run() external returns (address) {
-        vm.startBroadcast();
         // TODO put inside a shared utility function.
         string memory config = vm.readFile("config/config.json");
         address owner = config.readAddress(".owner");
         address upgrader = config.readAddress(".upgrader");
         address createxFactory = config.readAddress(".createxFactory");
-        string memory chain = vm.envString("CHAIN"); // e.g. "sepolia" or "arbitrum_sepolia"
+        string memory chain = vm.envString("CHAIN"); // the same name as the config file.
         string memory prefix = string.concat(".chains.", chain);
         bytes32 createxSalt = config.readBytes32(string.concat(prefix, ".rlcCrossChainTokenCreatexSalt"));
+        vm.startBroadcast();
         address rlcCrosschainTokenProxy = deploy("RLC Token", "RLC", owner, upgrader, createxFactory, createxSalt);
         vm.stopBroadcast();
         // TODO save contract address.
         return rlcCrosschainTokenProxy;
     }
 
+    /**
+     * Deploys the RLCCrosschainToken proxy using CreateX.
+     *
+     * @param name The name of the token.
+     * @param symbol The symbol of the token.
+     * @param owner The address of the owner.
+     * @param upgrader The address with upgrade permissions.
+     * @param createxFactory The CreateX factory address.
+     * @param createxSalt The salt for CreateX deployment.
+     * @return address of the deployed RLCCrosschainToken proxy contract.
+     */
     function deploy(
         string memory name,
         string memory symbol,
