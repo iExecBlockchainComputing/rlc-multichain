e0c115b3b33310094e1df8a30b5f83a165a5f4e7
Zied <26070035+zguesmi@users.noreply.github.com>
2025-06-16 14:57:23 +0200

    Remove unused import

--- test/units/RLCCrosschainTokenScript.t.sol
+++ test/units/RLCCrosschainTokenScript.t.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.22;
 
 import {Test, console} from "forge-std/Test.sol";
 import {CreateX} from "@createx/contracts/CreateX.sol";
-import {ICreateX} from "@createx/contracts/ICreateX.sol";
 import {Deploy as RLCCrosschainTokenDeployScript} from "../../script/RLCCrosschainToken.s.sol";
 import {RLCCrosschainToken} from "../../src/token/RLCCrosschainToken.sol";
 
