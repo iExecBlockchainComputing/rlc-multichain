3feb507f6ba76c4ee58e1ff5c65815f58d1add6d
Zied <26070035+zguesmi@users.noreply.github.com>
2025-06-16 13:07:33 +0200

    Add create2 conflicting deployment test

--- test/units/RLCCrosschainTokenScript.t.sol
+++ test/units/RLCCrosschainTokenScript.t.sol
@@ -10,15 +10,16 @@ import {Deploy as RLCCrosschainTokenDeployScript} from "../../script/RLCCrosscha
 import {RLCCrosschainToken} from "../../src/token/RLCCrosschainToken.sol";
 
 contract RLCCrosschainTokenTest is Test {
+    address private createx = address(new CreateX());
+    bytes32 private salt = keccak256("salt");
     address private owner = makeAddr("owner");
     address private upgrader = makeAddr("upgrader");
+    RLCCrosschainTokenDeployScript private deployer = new RLCCrosschainTokenDeployScript();
 
     function setUp() public {}
 
     function test_Deploy() public {
-        address crosschainTokenAddress = new RLCCrosschainTokenDeployScript().deploy(
-            "RLC Token", "RLC", owner, upgrader, address(new CreateX()), keccak256("salt")
-        );
+        address crosschainTokenAddress = deployer.deploy("RLC Token", "RLC", owner, upgrader, createx, salt);
         RLCCrosschainToken crossChainToken = RLCCrosschainToken(crosschainTokenAddress);
         assertEq(crossChainToken.name(), "RLC Token");
         assertEq(crossChainToken.symbol(), "RLC");
@@ -26,5 +27,14 @@ contract RLCCrosschainTokenTest is Test {
         assertEq(crossChainToken.hasRole(crossChainToken.DEFAULT_ADMIN_ROLE(), owner), true);
         assertEq(crossChainToken.hasRole(crossChainToken.UPGRADER_ROLE(), upgrader), true);
         // TODO check that the proxy address is saved.
+    }
+
+    // Makes sure create2 deployment is well implemented.
+    function test_RevertWhenTwoDeploymentsWithTheSameSalt() public {
+        console.log("CreateX address:", createx);
+        address random = makeAddr("random");
+        deployer.deploy("RLC Token", "RLC", owner, upgrader, createx, salt);
+        vm.expectRevert(abi.encodeWithSignature("FailedContractCreation(address)", createx));
+        deployer.deploy("Foo", "BAR", random, random, createx, salt);
     }
 }
