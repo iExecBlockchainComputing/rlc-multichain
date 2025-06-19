a2ca4545b9347ebcebc8772283a1893a223afc4c
gfournieriExec <gabriel.fournier@iex.ec>
2025-06-19 22:27:24 +0200

    fix: forge fmt

--- src/interfaces/IIexecLayerZeroBridge.sol
+++ src/interfaces/IIexecLayerZeroBridge.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.22;
 
-
 interface IIexecLayerZeroBridge {
     /**
      * @notice Pauses the contract, disabling `_credit` & `_debit` functions.
@@ -20,7 +19,7 @@ interface IIexecLayerZeroBridge {
      * @dev Should only be callable by authorized accounts: PAUSER_ROLE.
      */
     function pauseSend() external;
-    
+
     /**
      * @notice Unpauses the `_debit` function, allowing outgoing transfers again.
      * @dev Should only be callable by authorized accounts: PAUSER_ROLE.
