e25d0d14b35d613a1b034e6407c4b943c4e96184
Zied <26070035+zguesmi@users.noreply.github.com>
2025-06-23 12:32:58 +0200

    Remove fuzz test for createx salt

--- test/e2e/RLCAdapterScript.t.sol
+++ test/e2e/RLCAdapterScript.t.sol
@@ -11,6 +11,7 @@ contract RLCAdapterScriptTest is Test {
     address owner = makeAddr("OWNER_ADDRESS");
     address pauser = makeAddr("PAUSER_ADDRESS");
     address RLC_TOKEN = 0x26A738b6D33EF4D94FF084D3552961b8f00639Cd;
+    address createx = 0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed;
 
     RLCAdapterDeploy public deployer;
 
@@ -18,7 +19,7 @@ contract RLCAdapterScriptTest is Test {
         // TODO use vm.rpcUrl("sepolia")
         vm.createSelectFork(vm.envString("SEPOLIA_RPC_URL"));
         deployer = new RLCAdapterDeploy();
-        vm.setEnv("CREATE_X_FACTORY", "0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed");
+        vm.setEnv("CREATE_X_FACTORY", vm.toString(createx));
     }
 
     // ============ Deployment Tests ============
@@ -33,28 +34,14 @@ contract RLCAdapterScriptTest is Test {
         // TODO check that the contract has been initialized and cannot be re-initialized.
         // TODO check that the contract has the correct LayerZero endpoint.
         // TODO check that the proxy address is saved.
-    }
 
-    function testForkFuzz_DifferentSaltsProduceDifferentAddresses(bytes32 salt1, bytes32 salt2) public {
-        vm.assume(salt1 != salt2); // ensure they are different
 
-        address addr1 = deployer.deploy(lzEndpoint, owner, pauser, salt1, RLC_TOKEN);
-        address addr2 = deployer.deploy(lzEndpoint, owner, pauser, salt2, RLC_TOKEN);
-
-        assertTrue(addr1 != addr2, "Fuzz test failed: different salts produced same address");
-    }
-
-    function testForkFuzz_RevertIfSecondDeploymentWithSameSalt(bytes32 salt) public {
-        // First deployment
-        address addr = deployer.deploy(lzEndpoint, owner, pauser, salt, RLC_TOKEN);
-        assertTrue(addr != address(0), "First deployment should succeed");
-
-        // Attempt redeployment with the same salt
-        try deployer.deploy(lzEndpoint, owner, pauser, salt, RLC_TOKEN) returns (address) {
-            revert("Expected revert on redeployment with same salt but no revert occurred");
-        } catch {
-            // Expected: revert due to CREATE2 address collision
-        }
+    // Makes sure create2 deployment is well implemented.
+    function test_RevertWhen_TwoDeploymentsWithTheSameSalt() public {
+        bytes32 salt = keccak256("salt");
+        deployer.deploy(lzEndpoint, owner, pauser, salt, RLC_TOKEN);
+        vm.expectRevert(abi.encodeWithSignature("FailedContractCreation(address)", createx));
+        deployer.deploy(lzEndpoint, owner, pauser, salt, RLC_TOKEN);
     }
 
     // TODO add tests for the configuration script.
