19aa1ed4cb4902cde32a18c4735174ca72099a45
Zied <26070035+zguesmi@users.noreply.github.com>
2025-06-23 15:06:53 +0200

    chore: fix solidity file headers

--- test/e2e/RLCAdapterScript.t.sol
+++ test/e2e/RLCAdapterScript.t.sol
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-FileCopyrightText: 2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-License-Identifier: Apache-2.0
+
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
