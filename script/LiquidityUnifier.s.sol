a82f28471030d65c18b3879c63680ba9855dc34d
Le-Caignec <robinlecaignec@yahoo.fr>
2025-06-23 14:36:48 +0200

    Implement code changes to enhance functionality and improve performance

--- script/LiquidityUnifier.s.sol
+++ script/LiquidityUnifier.s.sol
@@ -29,31 +29,34 @@ contract Deploy is Script {
         address createxFactory = config.readAddress(".createxFactory");
         string memory chain = vm.envString("CHAIN"); // the same name as the config file.
         string memory prefix = string.concat(".chains.", chain);
+        address rlcToken = config.readAddress(string.concat(prefix, ".rlcAddress"));
         bytes32 createxSalt = config.readBytes32(string.concat(prefix, ".liquidityUnifierCreatexSalt"));
         vm.startBroadcast();
-        address liquidityUnifierProxy = deploy(admin, upgrader, createxFactory, createxSalt);
+        address liquidityUnifierProxy = deploy(rlcToken,admin, upgrader, createxFactory, createxSalt);
         vm.stopBroadcast();
 
         //TODO: use config file to store addresses.
-        EnvUtils.updateEnvVariable("RLC_CROSSCHAIN_ADDRESS", liquidityUnifierProxy);
+        EnvUtils.updateEnvVariable("LIQUIDITY_UNIFIER_PROXY_ADDRESS", liquidityUnifierProxy);
         return liquidityUnifierProxy;
     }
 
     /**
      * Deploys the LiquidityUnifier proxy using CreateX.
      *
+     * * @param rlcToken The address of the RLC token contract.
      * @param admin The address of the admin.
      * @param upgrader The address with upgrade permissions.
      * @param createxFactory The CreateX factory address.
      * @param createxSalt The salt for CreateX deployment.
      * @return address of the deployed LiquidityUnifier proxy contract.
      */
-    function deploy(address admin, address upgrader, address createxFactory, bytes32 createxSalt)
+    function deploy(address rlcToken,address admin, address upgrader, address createxFactory, bytes32 createxSalt)
         public
         returns (address)
     {
+        bytes memory constructorData = abi.encode(rlcToken);
         bytes memory initData = abi.encodeWithSelector(LiquidityUnifier.initialize.selector, admin, upgrader);
         return
-            UUPSProxyDeployer.deployUUPSProxyWithCreateX("LiquidityUnifier", "", initData, createxFactory, createxSalt);
+            UUPSProxyDeployer.deployUUPSProxyWithCreateX("LiquidityUnifier", constructorData, initData, createxFactory, createxSalt);
     }
 }
