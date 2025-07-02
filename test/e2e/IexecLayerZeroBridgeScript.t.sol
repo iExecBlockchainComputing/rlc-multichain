8429a1982ed0947da77d58853e2c93b2d09aab06
Le-Caignec <robinlecaignec@yahoo.fr>
2025-07-02 10:35:33 +0200

    test: add comprehensive deployment tests for IexecLayerZeroBridge on Ethereum Mainnet and Arbitrum

--- test/e2e/IexecLayerZeroBridgeScript.t.sol
+++ test/e2e/IexecLayerZeroBridgeScript.t.sol
@@ -7,15 +7,17 @@ import {Test} from "forge-std/Test.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {Deploy as IexecLayerZeroBridgeDeploy} from "../../script/bridges/layerZero/IexecLayerZeroBridge.s.sol";
 import {Deploy as RLCLiquidityUnifierDeployScript} from "../../script/RLCLiquidityUnifier.s.sol";
+import {Deploy as RLCCrosschainTokenDeployScript} from "../../script/RLCCrosschainToken.s.sol";
 import {IexecLayerZeroBridge} from "../../src/bridges/layerZero/IexecLayerZeroBridge.sol";
 import {RLCLiquidityUnifier} from "../../src/RLCLiquidityUnifier.sol";
+import {RLCCrosschainToken} from "../../src/RLCCrosschainToken.sol";
 
 /**
  * Test Script for the IexecLayerZeroBridge on Ethereum Mainnet.
  * In this case, IexecLayerZeroBridge should be connected to
  * RLCLiquidityUnifier contract deployed on the same chain.
  */
-contract IexecLayerZeroBridgeScriptOnMainnetTest is Test {
+contract IexecLayerZeroBridgeScriptTest is Test {
     // TODO read value from config.json file.
     address LAYERZERO_ENDPOINT = 0x6EDCE65403992e310A62460808c4b910D972f10f; // LayerZero Arbitrum Sepolia endpoint
     address CREATEX = 0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed;
@@ -25,32 +27,77 @@ contract IexecLayerZeroBridgeScriptOnMainnetTest is Test {
     address upgrader = makeAddr("upgrader");
     address pauser = makeAddr("pauser");
     bytes32 salt = keccak256("salt");
-    bool requireApproval = true;
 
     IexecLayerZeroBridgeDeploy public deployer;
-    RLCLiquidityUnifier private liquidityUnifier;
+    address private liquidityUnifier;
+    address private rlcCrosschain;
+
+    // Forks ID
+    uint256 private ethereumMainnetFork;
+    uint256 private arbitrumFork;
 
     function setUp() public {
-        vm.createSelectFork(vm.envString("SEPOLIA_RPC_URL"));
         deployer = new IexecLayerZeroBridgeDeploy();
-        liquidityUnifier = RLCLiquidityUnifier(
-            new RLCLiquidityUnifierDeployScript().deploy(
-                address(rlcAddress), admin, upgrader, CREATEX, keccak256("salt")
-            )
+
+        // Create a forks
+        ethereumMainnetFork = vm.createFork(vm.envString("SEPOLIA_RPC_URL"));
+        arbitrumFork = vm.createFork(vm.envString("ARBITRUM_SEPOLIA_RPC_URL"));
+
+        // Setup Ethereum Mainnet fork
+        vm.selectFork(ethereumMainnetFork);
+        liquidityUnifier = new RLCLiquidityUnifierDeployScript().deploy(
+            address(rlcAddress), admin, upgrader, CREATEX, keccak256("salt")
         );
+
+        // Setup Arbitrum Sepolia fork
+        vm.selectFork(arbitrumFork);
+        rlcCrosschain =
+            new RLCCrosschainTokenDeployScript().deploy("iEx.ec Network Token", "RLC", admin, admin, CREATEX, salt);
     }
 
-    function testFork_Deployment() public {
+    // ###############################################
+    // Without ApprovalRequired
+    // ###############################################
+
+    function testFork_DeploymentOnChainWithoutApproval() public {
+        vm.selectFork(arbitrumFork);
+        _testDeployment(false, rlcCrosschain);
+    }
+
+    function testFork_RevertWhen_TwoDeploymentsWithTheSameSaltWithoutApproval() public {
+        vm.selectFork(arbitrumFork);
+        _testTwoDeploymentsWithTheSameSalt(false, rlcCrosschain);
+    }
+
+    // ###############################################
+    // With ApprovalRequired
+    // ###############################################
+
+    function testFork_DeploymentOnChainWithApproval() public {
+        vm.selectFork(ethereumMainnetFork);
+        _testDeployment(true, liquidityUnifier);
+    }
+
+    function testFork_RevertWhen_TwoDeploymentsWithTheSameSaltWithApproval() public {
+        vm.selectFork(ethereumMainnetFork);
+        _testTwoDeploymentsWithTheSameSalt(true, liquidityUnifier);
+    }
+
+    // ###############################################
+    // Common functions
+    // ###############################################
+
+    function _testDeployment(bool _requireApproval, address bridgeableToken) internal {
         IexecLayerZeroBridge iexecLayerZeroBridge = IexecLayerZeroBridge(
             deployer.deploy(
-                requireApproval, address(liquidityUnifier), LAYERZERO_ENDPOINT, admin, upgrader, pauser, CREATEX, salt
+                _requireApproval, bridgeableToken, LAYERZERO_ENDPOINT, admin, upgrader, pauser, CREATEX, salt
             )
         );
 
         assertEq(iexecLayerZeroBridge.owner(), admin);
-        assertEq(iexecLayerZeroBridge.token(), address(rlcAddress));
+        assertEq(iexecLayerZeroBridge.token(), _requireApproval ? rlcAddress : bridgeableToken);
         // Check ApprovalRequired value
-        assertEq(iexecLayerZeroBridge.approvalRequired(), requireApproval, "ApprovalRequired should be false");
+        assertEq(iexecLayerZeroBridge.approvalRequired(), _requireApproval, "Incorrect ApprovalRequired value");
         // Check all roles.
         assertTrue(iexecLayerZeroBridge.hasRole(iexecLayerZeroBridge.DEFAULT_ADMIN_ROLE(), admin));
         assertTrue(iexecLayerZeroBridge.hasRole(iexecLayerZeroBridge.UPGRADER_ROLE(), upgrader));
@@ -64,10 +111,10 @@ contract IexecLayerZeroBridgeScriptOnMainnetTest is Test {
         // TODO check that the proxy address is saved.
     }
 
-    function testFork_RevertWhen_TwoDeploymentsWithTheSameSalt() public {
-        deployer.deploy(requireApproval, rlcAddress, LAYERZERO_ENDPOINT, admin, upgrader, pauser, CREATEX, salt);
+    function _testTwoDeploymentsWithTheSameSalt(bool _requireApproval, address bridgeableToken) internal {
+        deployer.deploy(_requireApproval, bridgeableToken, LAYERZERO_ENDPOINT, admin, upgrader, pauser, CREATEX, salt);
         vm.expectRevert(abi.encodeWithSignature("FailedContractCreation(address)", CREATEX));
-        deployer.deploy(requireApproval, rlcAddress, LAYERZERO_ENDPOINT, admin, upgrader, pauser, CREATEX, salt);
+        deployer.deploy(_requireApproval, bridgeableToken, LAYERZERO_ENDPOINT, admin, upgrader, pauser, CREATEX, salt);
     }
 
     // TODO add tests for the configuration script.
