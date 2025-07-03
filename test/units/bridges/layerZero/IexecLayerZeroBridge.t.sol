60d310194b5e17efe0f793242b1b5fa59245369e
Le-Caignec <robinlecaignec@yahoo.fr>
2025-07-03 12:01:18 +0200

    refactor: streamline pause and approval tests in IexecLayerZeroBridgeTest

--- test/units/bridges/layerZero/IexecLayerZeroBridge.t.sol
+++ test/units/bridges/layerZero/IexecLayerZeroBridge.t.sol
@@ -85,106 +85,14 @@ contract IexecLayerZeroBridgeTest is TestHelperOz5 {
     //TODO: Add fuzzing to test sharedDecimals and sharedDecimalsRounding issues
     //TODO: Add more tests for send functionality, in both directions
 
-    // ###############################################
-    // With ApprovalRequired
-    // ###############################################
-
     // ============ BASIC BRIDGE FUNCTIONALITY TESTS ============
     function test_SendToken_WhenOperational_WithApproval() public {
         _test_SendToken_WhenOperational(iexecLayerZeroBridgeEthereum, address(rlcToken), true);
     }
 
-    // ============ LEVEL 1 PAUSE TESTS (Complete Pause) ============
-    function test_Pause_OnlyPauserRole_WithApproval() public {
-        _test_Pause_OnlyPauserRole(iexecLayerZeroBridgeEthereum);
-    }
-
-    function test_Pause_BlocksOutgoingTransfers__WithApproval() public {
-        _test_Pause_BlocksOutgoingTransfers(iexecLayerZeroBridgeEthereum, address(rlcToken), true);
-    }
-
-    function test_Unpause_RestoresFullFunctionality_WithApproval() public {
-        _test_Unpause_RestoresFullFunctionality(iexecLayerZeroBridgeEthereum, address(rlcToken), true);
-    }
-
-    function test_sendRLCWhenSourceLayerZeroBridgeUnpaused_WithApproval() public {
-        _test_sendRLCWhenSourceLayerZeroBridgeUnpaused(iexecLayerZeroBridgeEthereum, address(rlcToken), true);
-    }
-
-    // ============ LEVEL 2 PAUSE TESTS (Send Pause) ============
-    function test_PauseSend_OnlyPauserRole_WithApproval() public {
-        _test_PauseSend_OnlyPauserRole(iexecLayerZeroBridgeEthereum);
-    }
-
-    function test_PauseSend_BlocksOutgoingOnly_WithApproval() public {
-        _test_PauseSend_BlocksOutgoingOnly(iexecLayerZeroBridgeEthereum, address(rlcToken), true);
-    }
-
-    function test_UnpauseSend_RestoresOutgoingTransfers_WithApproval() public {
-        _test_UnpauseSend_RestoresOutgoingTransfers(iexecLayerZeroBridgeEthereum, address(rlcToken), true);
-    }
-
-    // ============ token and approvalRequired ============
-
-    function test_ReturnsApprovalRequired_WithApproval() public {
-        _testReturnsApprovalRequired(iexecLayerZeroBridgeEthereum, true);
-    }
-
-    function test_ReturnsBridgeableTokenAddress_WithApproval() public view {
-        _testBridgeableTokenAddress(iexecLayerZeroBridgeEthereum, address(rlcToken));
-    }
-
-    // ###############################################
-    // Without ApprovalRequired
-    // ###############################################
-
-    // ============ BASIC BRIDGE FUNCTIONALITY TESTS ============
     function test_SendToken_WhenOperational_WithoutApproval() public {
         _test_SendToken_WhenOperational(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken), false);
     }
-
-    // ============ LEVEL 1 PAUSE TESTS (Complete Pause) ============
-    function test_Pause_OnlyPauserRole_WithoutApproval() public {
-        _test_Pause_OnlyPauserRole(iexecLayerZeroBridgeChainX);
-    }
-
-    function test_Pause_BlocksOutgoingTransfers_WithoutApproval() public {
-        _test_Pause_BlocksOutgoingTransfers(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken), false);
-    }
-
-    function test_Unpause_RestoresFullFunctionality_WithoutApproval() public {
-        _test_Unpause_RestoresFullFunctionality(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken), false);
-    }
-
-    function test_sendRLCWhenSourceLayerZeroBridgeUnpaused_WithoutApproval() public {
-        _test_sendRLCWhenSourceLayerZeroBridgeUnpaused(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken), false);
-    }
-
-    // ============ LEVEL 2 PAUSE TESTS (Send Pause) ============
-    function test_PauseSend_OnlyPauserRole_WithoutApproval() public {
-        _test_PauseSend_OnlyPauserRole(iexecLayerZeroBridgeChainX);
-    }
-
-    function test_PauseSend_BlocksOutgoingOnly_WithoutApproval() public {
-        _test_PauseSend_BlocksOutgoingOnly(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken), false);
-    }
-
-    function test_UnpauseSend_RestoresOutgoingTransfers_WithoutApproval() public {
-        _test_UnpauseSend_RestoresOutgoingTransfers(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken), false);
-    }
-
-    // ============ token and approvalRequired ============
-    function test_ReturnsApprovalRequired_WithoutApproval() public {
-        _testReturnsApprovalRequired(iexecLayerZeroBridgeChainX, false);
-    }
-
-    function test_ReturnsBridgeableTokenAddress_WithoutApproval() public view {
-        _testBridgeableTokenAddress(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken));
-    }
-
-    // ###############################################
-    // Common functions
-    // ###############################################
 
     function _test_SendToken_WhenOperational(
         IexecLayerZeroBridge iexecLayerZeroBridge,
@@ -218,143 +126,118 @@ contract IexecLayerZeroBridgeTest is TestHelperOz5 {
         assertEq(token.balanceOf(user1), INITIAL_BALANCE - TRANSFER_AMOUNT, "Tokens should be deducted from sender");
     }
 
-    function _test_Pause_OnlyPauserRole(IexecLayerZeroBridge iexecLayerZeroBridge) internal {
+    // ============ LEVEL 1 PAUSE TESTS (Complete Pause) ============
+    function test_Pause_OnlyPauserRole() public {
         vm.expectRevert();
         vm.prank(unauthorizedUser);
-        iexecLayerZeroBridge.pause();
+        iexecLayerZeroBridgeChainX.pause();
     }
 
-    function _test_Pause_BlocksOutgoingTransfers(
-        IexecLayerZeroBridge iexecLayerZeroBridge,
-        address tokenAddress,
-        bool approvalRequired
-    ) public {
-        // This interface can be use for both token as we only use balanceOf func
-        RLCMock token = RLCMock(tokenAddress);
-
+    function test_Pause_BlocksOutgoingTransfers() public {
         // Pause the bridge
         vm.prank(pauser);
-        iexecLayerZeroBridge.pause();
+        iexecLayerZeroBridgeChainX.pause();
 
         // Prepare send parameters
-        (SendParam memory sendParam, MessagingFee memory fee) = TestUtils.prepareSend(
-            iexecLayerZeroBridge, addressToBytes32(user2), TRANSFER_AMOUNT, approvalRequired ? DEST_EID : SOURCE_EID
-        );
+        (SendParam memory sendParam, MessagingFee memory fee) =
+            TestUtils.prepareSend(iexecLayerZeroBridgeChainX, addressToBytes32(user2), TRANSFER_AMOUNT, SOURCE_EID);
 
         // Attempt to send tokens - should revert
         vm.deal(user1, fee.nativeFee);
         vm.prank(user1);
         vm.expectRevert(PausableUpgradeable.EnforcedPause.selector);
-        iexecLayerZeroBridge.send{value: fee.nativeFee}(sendParam, fee, payable(user1));
+        iexecLayerZeroBridgeChainX.send{value: fee.nativeFee}(sendParam, fee, payable(user1));
 
         // Verify no tokens were burned
-        assertEq(token.balanceOf(user1), INITIAL_BALANCE);
+        assertEq(rlcCrosschainToken.balanceOf(user1), INITIAL_BALANCE);
     }
 
-    function _test_Unpause_RestoresFullFunctionality(
-        IexecLayerZeroBridge iexecLayerZeroBridge,
-        address tokenAddress,
-        bool approvalRequired
-    ) internal {
+    function test_Unpause_RestoresFullFunctionality() public {
         // Pause then unpause the bridge
         vm.startPrank(pauser);
-        iexecLayerZeroBridge.pause();
+        iexecLayerZeroBridgeChainX.pause();
 
-        iexecLayerZeroBridge.unpause();
+        iexecLayerZeroBridgeChainX.unpause();
         vm.stopPrank();
 
         // Should now work normally
-        _test_SendToken_WhenOperational(iexecLayerZeroBridge, tokenAddress, approvalRequired);
+        test_SendToken_WhenOperational_WithoutApproval();
     }
 
-    function _test_sendRLCWhenSourceLayerZeroBridgeUnpaused(
-        IexecLayerZeroBridge iexecLayerZeroBridge,
-        address tokenAddress,
-        bool approvalRequired
-    ) public {
-        // This interface can be use for both token as we only use balanceOf func
-        RLCMock token = RLCMock(tokenAddress);
-
+    function test_sendRLCWhenSourceLayerZeroBridgeUnpaused() public {
         // Pause then unpause the bridge
         vm.startPrank(pauser);
-        iexecLayerZeroBridge.pause();
-        iexecLayerZeroBridge.unpause();
+        iexecLayerZeroBridgeChainX.pause();
+        iexecLayerZeroBridgeChainX.unpause();
         vm.stopPrank();
 
         // Prepare send parameters using utility
-        (SendParam memory sendParam, MessagingFee memory fee) = TestUtils.prepareSend(
-            iexecLayerZeroBridge, addressToBytes32(user2), TRANSFER_AMOUNT, approvalRequired ? DEST_EID : SOURCE_EID
-        );
+        (SendParam memory sendParam, MessagingFee memory fee) =
+            TestUtils.prepareSend(iexecLayerZeroBridgeChainX, addressToBytes32(user2), TRANSFER_AMOUNT, SOURCE_EID);
 
         // Send tokens
         vm.deal(user1, fee.nativeFee);
-        vm.startPrank(user1);
-        if (approvalRequired) {
-            token.approve(address(iexecLayerZeroBridge), TRANSFER_AMOUNT); // For Stargate compatibility, user should approve iexecLayerZeroBridge
-        }
-        iexecLayerZeroBridge.send{value: fee.nativeFee}(sendParam, fee, payable(user1));
-        vm.stopPrank();
+        vm.prank(user1);
+        iexecLayerZeroBridgeChainX.send{value: fee.nativeFee}(sendParam, fee, payable(user1));
 
         // Verify source state - tokens should be burned
-        assertEq(token.balanceOf(user1), INITIAL_BALANCE - TRANSFER_AMOUNT);
+        assertEq(rlcCrosschainToken.balanceOf(user1), INITIAL_BALANCE - TRANSFER_AMOUNT);
     }
 
-    function _test_PauseSend_OnlyPauserRole(IexecLayerZeroBridge iexecLayerZeroBridge) public {
+    // ============ LEVEL 2 PAUSE TESTS (Send Pause) ============
+
+    function test_PauseSend_OnlyPauserRole() public {
         vm.expectRevert();
         vm.prank(unauthorizedUser);
-        iexecLayerZeroBridge.pauseSend();
+        iexecLayerZeroBridgeChainX.pauseSend();
     }
 
-    function _test_PauseSend_BlocksOutgoingOnly(
-        IexecLayerZeroBridge iexecLayerZeroBridge,
-        address tokenAddress,
-        bool approvalRequired
-    ) public {
-        // This interface can be use for both token as we only use balanceOf func
-        RLCMock token = RLCMock(tokenAddress);
-
+    function test_PauseSend_BlocksOutgoingOnly() public {
         // Pause send
         vm.prank(pauser);
-        iexecLayerZeroBridge.pauseSend();
+        iexecLayerZeroBridgeChainX.pauseSend();
 
         // Verify state
-        assertFalse(iexecLayerZeroBridge.paused());
-        assertTrue(iexecLayerZeroBridge.sendPaused());
+        assertFalse(iexecLayerZeroBridgeChainX.paused());
+        assertTrue(iexecLayerZeroBridgeChainX.sendPaused());
 
         // Prepare send parameters
-        (SendParam memory sendParam, MessagingFee memory fee) = TestUtils.prepareSend(
-            iexecLayerZeroBridge, addressToBytes32(user2), TRANSFER_AMOUNT, approvalRequired ? DEST_EID : SOURCE_EID
-        );
+        (SendParam memory sendParam, MessagingFee memory fee) =
+            TestUtils.prepareSend(iexecLayerZeroBridgeChainX, addressToBytes32(user2), TRANSFER_AMOUNT, SOURCE_EID);
 
         // Attempt to send tokens - should revert with EnforcedSendPause
         vm.deal(user1, fee.nativeFee);
         vm.prank(user1);
         vm.expectRevert(DualPausableUpgradeable.EnforcedSendPause.selector);
-        iexecLayerZeroBridge.send{value: fee.nativeFee}(sendParam, fee, payable(user1));
+        iexecLayerZeroBridgeChainX.send{value: fee.nativeFee}(sendParam, fee, payable(user1));
 
         // Verify no tokens were burned
-        assertEq(token.balanceOf(user1), INITIAL_BALANCE);
+        assertEq(rlcCrosschainToken.balanceOf(user1), INITIAL_BALANCE);
     }
 
-    function _test_UnpauseSend_RestoresOutgoingTransfers(
-        IexecLayerZeroBridge iexecLayerZeroBridge,
-        address tokenAddress,
-        bool approvalRequired
-    ) public {
+    function test_UnpauseSend_RestoresOutgoingTransfers() public {
         // Pause then unpause send
         vm.startPrank(pauser);
-        iexecLayerZeroBridge.pauseSend();
+        iexecLayerZeroBridgeChainX.pauseSend();
 
-        iexecLayerZeroBridge.unpauseSend();
+        iexecLayerZeroBridgeChainX.unpauseSend();
         vm.stopPrank();
 
         // Should now work normally
-        assertFalse(iexecLayerZeroBridge.paused());
-        assertFalse(iexecLayerZeroBridge.sendPaused());
+        assertFalse(iexecLayerZeroBridgeChainX.paused());
+        assertFalse(iexecLayerZeroBridgeChainX.sendPaused());
 
-        _test_SendToken_WhenOperational(iexecLayerZeroBridge, tokenAddress, approvalRequired);
+        test_SendToken_WhenOperational_WithoutApproval();
     }
 
+
+    // ============ token and approvalRequired ============
+    function test_ReturnsApprovalRequired_WithApproval() public {
+        _testReturnsApprovalRequired(iexecLayerZeroBridgeEthereum, true);
+    }
+    function test_ReturnsApprovalRequired_WithoutApproval() public {
+        _testReturnsApprovalRequired(iexecLayerZeroBridgeChainX, false);
+    }
     function _testReturnsApprovalRequired(IexecLayerZeroBridge iexecLayerZeroBridge, bool requireApproval) internal {
         requireApproval ? vm.chainId(1) : vm.chainId(42161);
         assertEq(
@@ -362,6 +245,14 @@ contract IexecLayerZeroBridgeTest is TestHelperOz5 {
             requireApproval,
             "approvalRequired() should return the correct value depending on the chain"
         );
+    }
+
+    function test_ReturnsBridgeableTokenAddress_WithApproval() public view {
+        _testBridgeableTokenAddress(iexecLayerZeroBridgeEthereum, address(rlcToken));
+    }
+
+    function test_ReturnsBridgeableTokenAddress_WithoutApproval() public view {
+        _testBridgeableTokenAddress(iexecLayerZeroBridgeChainX, address(rlcCrosschainToken));
     }
 
     function _testBridgeableTokenAddress(IexecLayerZeroBridge iexecLayerZeroBridge, address tokenAddress)
