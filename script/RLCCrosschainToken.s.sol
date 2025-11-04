ca0b55c41a7b1a37bb732c3d6f8753507fff4670
gfournieriExec <gabriel.fournier@iex.ec>
2025-11-04 17:17:05 +0100

    refactor: Simplify initialization data encoding in deploy function

--- script/RLCCrosschainToken.s.sol
+++ script/RLCCrosschainToken.s.sol
@@ -56,9 +56,8 @@ contract Deploy is Script {
         address createxFactory,
         bytes32 createxSalt
     ) public returns (address) {
-        bytes memory initData = abi.encodeWithSelector(
-            RLCCrosschainToken.initialize.selector, name, symbol, initialAdmin, initialUpgrader
-        );
+        bytes memory initData =
+            abi.encodeWithSelector(RLCCrosschainToken.initialize.selector, name, symbol, initialAdmin, initialUpgrader);
         return UUPSProxyDeployer.deployUsingCreateX("RLCCrosschainToken", "", initData, createxFactory, createxSalt);
     }
 }
